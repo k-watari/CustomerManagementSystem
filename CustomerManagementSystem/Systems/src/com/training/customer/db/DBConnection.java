54119960b5b36ff9b2c3f39ef4c0dfb6d8be821e
k-watari <k-watari@keybind.com>
2026-09-10 15:49:04 +0900

    4回目のコミット : DBConnectionの追加

---
+++ CustomerManagementSystem/Systems/src/com/training/customer/db/DBConnection.java
@@ -0,0 +1,18 @@
+package com.training.customer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    
+    private static final String URL = 
+        "jdbc:mysql://localhost:3306/Week6db?useSSL=false&allwoPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo";
+    private static final String USER = "user";
+    private static final String PASSWORD = "pass";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
